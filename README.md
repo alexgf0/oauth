1f5e3784f8a338194c3f3542143b0eac981f68df
alexgf0 <aleexgf0@gmail.com>
2023-11-05 10:45:45 +0100

    Redirect endpoint

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Open Authorization used for Authenitcation in Rust Web Server
+This repository implements a basic Rust server that uses OAuth2 to obtain the gmail from a Google user.
+The implementation avoids the use of frameworks(regarding OAuth functionality) to exemplify the minimum basic OAuth workflow.
+
+For purposes of brevity, neither tests nor error handling has been implemented.
+
+# Interesting references
+[OAuth workflow by IBM](https://www.ibm.com/docs/en/tfim/6.2.2.6?topic=overview-oauth-20-workflow)
+
+[Google's good documentation](https://developers.google.com/identity/protocols/oauth2/web-server#httprest_1)
+
+
+# Steps to get google user data
+## Enabling OAuth from your Google account
+[OAuth Google Configuration](https://console.cloud.google.com/apis/dashboard)
+
+## Get user authorization code
+
+## Get access token using the authorization code
+we had problems with the timing and reuse of the authorization code given...
+
+
+## Use the access tokens to get google's user information
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,48 @@
 use actix_web::{middleware::Logger, App, HttpServer};
-use actix_files::Files;
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder, HttpRequest};
+use dotenv::dotenv;
 
-#[get("/test")]
-async fn test_endpoint() -> impl Responder {
-    HttpResponse::Ok().body("Hi") 
+
+
+#[get("/login")]
+async fn google_login() -> impl Responder {
+    // get the parameters from .env file to make get petition to google
+    let redirect_uri = std::env::var("google_redirect_uri").expect("google_redirect_uri must be defined in the .env file.");
+    let client_id = std::env::var("google_client_id").expect("google_client_id must be defined in the .env file.");
+    let scope = std::env::var("google_scope").expect("google_scope must be defined in the .env file.");
+
+
+    let body = format!("<a href=\"https://accounts.google.com/o/oauth2/v2/auth?scope={}&redirect_uri={}&response_type=code&client_id={}\">Google Login</a>",
+        scope, redirect_uri, client_id);
+
+    HttpResponse::Ok().body(body)
+}
+
+#[get("/google/redirect")]
+async fn google_redirect(req_headers: HttpRequest) -> impl Responder {
+    let parameters = req_headers.query_string().split("&");
+    for parameter in parameters {
+        let values = parameter.split("=").collect::<Vec<&str>>();
+
+        if values[0] == "code" {
+            println!("Your authorization code is: {}", values[1]);
+        }
+    }
+    HttpResponse::Ok().body(format!("Hi"))
 }
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
     HttpServer::new(|| {
         App::new()
-            .service(test_endpoint)
-            //termporary to test the development of the database
-            // We allow the visitor to see an index of the images at `/images`.
-            //.service(Files::new("/images", "static/images/").show_files_listing())
-            // We allow the visitor to see an index of the javascript files at `/js`.
-            .service(Files::new("/js", "main/static/js/").show_files_listing())
-            .service(Files::new("/imgs", "main/static/imgs").show_files_listing())
-            .service(Files::new("/css", "main/static/css").show_files_listing())
+            .service(google_login)
+            .service(google_redirect)
             // Enable the logger.
             .wrap(Logger::default())
     })
-    //.bind(("178.79.142.51", 80))?
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
