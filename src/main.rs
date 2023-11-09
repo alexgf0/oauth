3a06e0b391ac9e59783c4bce8bc22842cfd9a4db
alexgf0 <aleexgf0@gmail.com>
2023-11-09 10:23:02 +0100

    Obtain access token

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{middleware::Logger, App, HttpServer};
 use actix_web::{get, HttpResponse, Responder, HttpRequest};
+use reqwest;
 use dotenv::dotenv;
 
 
@@ -18,6 +19,31 @@ async fn google_login() -> impl Responder {
     HttpResponse::Ok().body(body)
 }
 
+async fn get_access_token(authorization_code: &str) -> String {
+    let client = reqwest::Client::builder().build().unwrap();
+    let client_id = std::env::var("google_client_id").expect("google_client_id must be defined in the .env file.");
+    let client_secret = std::env::var("google_client_secret").expect("google_client_secret must be defined in the .env file.");
+    let redirect_uri = std::env::var("google_redirect_uri").expect("google_redirect_uri must be defined in the .env file.");
+
+
+    //send get request to https://www.googleapis.com/oauth2/v4/token with our parameters set in the url
+    let request = format!("https://www.googleapis.com/oauth2/v4/token?code={}&client_id={}&client_secret={}&grant_type=authorization_code&redirect_uri={}",
+        authorization_code, client_id, client_secret, redirect_uri);
+
+    let request = client
+        .request(reqwest::Method::POST, request)
+        .header("Content-Length", "0");
+    
+    let response = request.send().await.unwrap();
+
+    let body = response.text().await.unwrap();
+    let body: serde_json::Value = serde_json::from_str(&body).unwrap();
+
+    body["access_token"].to_string()
+}
+
+
+
 #[get("/google/redirect")]
 async fn google_redirect(req_headers: HttpRequest) -> impl Responder {
     let parameters = req_headers.query_string().split("&");
@@ -25,7 +51,9 @@ async fn google_redirect(req_headers: HttpRequest) -> impl Responder {
         let values = parameter.split("=").collect::<Vec<&str>>();
 
         if values[0] == "code" {
-            println!("Your authorization code is: {}", values[1]);
+            println!("calling access_token with {}", values[1]);
+            let access_token = get_access_token(values[1]).await;
+            println!("access_token: {access_token}");
         }
     }
     HttpResponse::Ok().body(format!("Hi"))
